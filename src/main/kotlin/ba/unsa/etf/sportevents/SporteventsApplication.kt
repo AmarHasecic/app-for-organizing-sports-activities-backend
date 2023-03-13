e1e44051ac556f9563f357560000997d56995af5
AmarHasecic <ahasecic1@etf.unsa.ba>
2023-03-13 14:38:44 +0100

    Initial commit

---
+++ src/main/kotlin/ba/unsa/etf/sportevents/SporteventsApplication.kt
@@ -0,0 +1,11 @@
+package ba.unsa.etf.sportevents
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SporteventsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SporteventsApplication>(*args)
+}
