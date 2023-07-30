8829edfad401dfcfa62fe0b721f6893d3134759e
AmarHasecic <ahasecic1@etf.unsa.ba>
2023-07-30 13:34:50 +0200

    deploy

--- src/main/kotlin/ba/unsa/etf/sportevents/SporteventsApplication.kt
+++ src/main/kotlin/ba/unsa/etf/sportevents/SporteventsApplication.kt
@@ -1,10 +1,8 @@
 package ba.unsa.etf.sportevents
 
-import io.jsonwebtoken.SignatureAlgorithm
-import io.jsonwebtoken.security.Keys
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import java.util.*
+
 
 @SpringBootApplication
 class SporteventsApplication
