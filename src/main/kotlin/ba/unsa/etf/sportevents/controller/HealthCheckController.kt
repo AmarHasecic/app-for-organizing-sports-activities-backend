7eb1e09adf1fcb1c7e08e075bd6bdc2cef51797a
AmarHasecic <ahasecic1@etf.unsa.ba>
2023-08-02 01:34:26 +0200

    deploy

---
+++ src/main/kotlin/ba/unsa/etf/sportevents/controller/HealthCheckController.kt
@@ -0,0 +1,15 @@
+package ba.unsa.etf.sportevents.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/health")
+class HealthCheckController {
+
+    @GetMapping
+    fun healthCheck(): String {
+        return "Application is running and healthy!"
+    }
+}
